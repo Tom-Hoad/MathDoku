96ed37e7282b7633960befc702a9141b653e3c9a
Tom-Hoad <tom.hoad03@gmail.com>
2020-02-24 17:14:53 +0000

    Added a new handler for key presses.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -16,6 +15,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
+
+import java.security.Key;
 
 public class Main extends Application {
 
@@ -50,6 +51,7 @@ public class Main extends Application {
         gridPane.setAlignment(Pos.CENTER);
 
         Rectangle[][] gridBoxes = new Rectangle[gridSize][gridSize];
+        gridPane.addEventHandler(KeyEvent.KEY_PRESSED, new KeyPressedHandler(gridSize));
 
         // Creates the grid.
         for (int x = 0; x < gridSize; x++) {
@@ -62,45 +64,76 @@ public class Main extends Application {
                 gridBox.setStrokeType(StrokeType.INSIDE);
                 gridBox.setFill(Color.WHITE);
                 gridBox.relocate(x * boxSize, y * boxSize);
+                gridPane.add(gridBox, x, y + 1);
 
                 // Deals with block clicks.
                 gridBoxes[x][y] = gridBox;
-                gridBox.addEventHandler(MouseEvent.MOUSE_CLICKED, new BoxClickHandler(gridBoxes, x, y));
-
-                gridPane.add(gridBox, x, y + 1);
+                gridBox.addEventHandler(MouseEvent.MOUSE_CLICKED, new BoxClickHandler(gridBoxes, x, y, gridPane));
             }
         }
         gridPane.add(optionsHBox, 0, 0, gridSize, 1);
 
         // Finishes setting up the GUI.
         Scene scene = new Scene(gridPane);
+
         stage.setScene(scene);
         stage.show();
     }
 
-    // Event handler code for click a grid button.
-    static class BoxClickHandler implements EventHandler<MouseEvent> {
+    // Event handler code for clicking a grid box.
+    class BoxClickHandler implements EventHandler<MouseEvent> {
         private Rectangle[][] gridBoxes;
         private int x;
         private int y;
+        private int gridSize;
+        private GridPane gridPane;
 
         // Event handler constructor.
-        public BoxClickHandler(Rectangle[][] gridBoxes, int x, int y) {
+        public BoxClickHandler(Rectangle[][] gridBoxes, int x, int y, GridPane gridPane) {
             this.gridBoxes = gridBoxes;
             this.x = x;
             this.y = y;
+            this.gridSize = gridBoxes.length;
+            this.gridPane = gridPane;
         }
 
         @Override
         public void handle(MouseEvent event) {
             // Highlights the currently selected grid box.
-            for (int i = 0; i < gridBoxes.length; i++) {
-                for (int j = 0; j < gridBoxes.length; j++) {
+            for (int i = 0; i < gridSize; i++) {
+                for (int j = 0; j < gridSize; j++) {
+                    Rectangle gridBox = gridBoxes[i][j];
+
                     if (i == x && j == y) {
-                        gridBoxes[i][j].setStrokeWidth(2.5);
-                    } else {
-                        gridBoxes[i][j].setStrokeWidth(1.0);
+                        gridBox.setStroke(Color.RED);
+                        gridBox.setStrokeWidth(2.5);
+                    } else if (gridBox.getStrokeWidth() != 1.0) {
+                        gridBox.setStroke(Color.BLACK);
+                        gridBox.setStrokeWidth(1.0);
                     }
+                }
+            }
+        }
+    }
+
+    // Event handler code for pressing a key.
+    class KeyPressedHandler implements EventHandler<KeyEvent> {
+        private int gridSize;
+
+        public KeyPressedHandler(int gridSize) {
+            this.gridSize = gridSize;
+        }
+
+        @Override
+        public void handle(KeyEvent event) {
+            // Returns the number pressed.
+            KeyCode key = event.getCode();
+            KeyCode[] codes = {KeyCode.DIGIT2, KeyCode.DIGIT3, KeyCode.DIGIT4, KeyCode.DIGIT5,
+                                KeyCode.DIGIT6, KeyCode.DIGIT7, KeyCode.DIGIT8};
+
+            for (int i = 0; i <= gridSize - 2; i++) {
+                if (key == codes[i]) {
+                    System.out.println("A valid number key was pressed.");
                 }
             }
         }
